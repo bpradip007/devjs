d78a3751bd0c4688af013aa2a126f0ebc63d93be
bpradip007 <78011346+bpradip007@users.noreply.github.com>
2021-12-29 11:52:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const {Logging} = require('@google-cloud/logging');
+const express = require('express');
+const app = express();
+const path = require('path');
+const got = require('got');
+
+app.get('/', async (req, res) => {
+  projectId = 'niveustraining', // Google Cloud Platform project ID
+  logName = 'demo' // The name of the log to write to
+  const logging = new Logging({projectId});
+  const log = logging.log(logName);
+  await log.write(log.entry({resource: {type: 'global'},severity: 'INFO'}, 'App recieved a request'));
+  try {
+    res
+      .sendFile(path.join(__dirname+'/index.html'))
+      .status(200);
+      //__dirname : It will resolve to your project folder.
+    await log.write(log.entry({resource: {type: 'global'},severity: 'INFO'}, 'Successfully rendered web page'));
+  }
+  catch (err) {
+    await log.write(log.entry({resource: {type: 'global'},severity: 'ERROR'},'Error'+err));
+    res
+      .status(500)
+      .end();
+  }
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('App listening on port', port);
+});
